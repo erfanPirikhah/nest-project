6aae886f70deee6d86a890638a20e5e668b33e68
erfan pirikhah <erfan.pirikhah1@gmial.com>
2023-05-17 13:49:47 +0330

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { LoggerMiddlewareMiddleware } from './logger-middleware/logger-middleware.middleware';
+
+@Module({
+  imports: [UsersModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddlewareMiddleware).forRoutes('*');
+  }
+}
